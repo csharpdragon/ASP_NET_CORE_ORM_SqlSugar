f38a5c79df3b0c0fd1fe8c45ba891d46acca049c
pdragon0512 <PDragon0512@gmail.com>
2022-07-10 17:19:14 -0700

    Initial commit

---
+++ SqlSugarCore/src/SqlSugarForCore/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("作者:孙凯旋，蓝灯软件架构师")]
+[assembly: AssemblyCompany("上海蓝灯软件有限公司")]
+[assembly: AssemblyProduct("SqlSugarForCore")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2ea1a286-e605-4eb8-ba44-c5861e8e552a")]
+[assembly: AssemblyVersion("3.5.3.4")]
+[assembly: AssemblyFileVersion("3.5.3.4")]
